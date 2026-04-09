8b6659a5175b4cd3ab38a236c375d889d2f61ae1
Jiří Fencl <jiri.fencl@firma.seznam.cz>
2026-04-09 11:28:34 +0200

    Commit

---
+++ .changeset/thin-suns-end.md
@@ -0,0 +1,10 @@
+---
+"@test-mono/router": patch
+"@test-mono/cli": patch
+---
+
+PAtch cli and repoter
+
+- **What** Update what
+- **Why** Update why
+- **How** Update how
--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -9,6 +9,4 @@ function runCli(args = []) {
     console.log(`${core.name} cli running with args:`, args);
 }
 
-// change comment to summary of the file
-
 module.exports = { runCli };
